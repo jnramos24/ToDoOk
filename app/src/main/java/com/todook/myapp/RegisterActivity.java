4b136e5f6740030308aa52c08c6242cf40ee10f8
Gaby Aguilera <gaby_ag@live.com.ar>
2023-09-30 14:00:05 -0300

    Agrego logica de navegabilidad

--- app/src/main/java/com/todook/myapp/RegisterActivity.java
+++ app/src/main/java/com/todook/myapp/RegisterActivity.java
@@ -2,13 +2,31 @@ package com.todook.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    ImageView iconVolver;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_main);
+
+        iconVolver = findViewById(R.id.icon_volver_R);
+        iconVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
