cef9545a6bd2173bd16e1a2c20f59c7da53dc94f
nelsonvilte <nelsonvilte@gmail.com>
2023-09-30 22:52:41 -0300

    add connection from main to contact

--- app/src/main/java/com/todook/myapp/MainActivity.java
+++ app/src/main/java/com/todook/myapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.todook.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnContact = findViewById(R.id.btn_contact);
+        btnContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ContactActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
