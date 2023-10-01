81c4cbbaa589f666cca3a93f498278d2850ed9c6
nelsonvilte <nelsonvilte@gmail.com>
2023-10-01 17:25:04 -0300

    update the register connection with login

--- app/src/main/java/com/todook/myapp/RegisterActivity.java
+++ app/src/main/java/com/todook/myapp/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -19,7 +20,17 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         iconVolver = findViewById(R.id.icon_volver_R);
+        Button btnRegister = findViewById(R.id.buttonRegister_R);
         iconVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+      btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
