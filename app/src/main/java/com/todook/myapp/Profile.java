c2441af027708df6253a86777cd5aa8fe191f06b
Daniela <daniela.barrios@dicsys.com>
2023-10-01 09:58:16 -0300

    volver al menú desde perfil

--- app/src/main/java/com/todook/myapp/Profile.java
+++ app/src/main/java/com/todook/myapp/Profile.java
@@ -1,14 +1,28 @@
 package com.todook.myapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Profile extends AppCompatActivity {
+
+    ImageView imgback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imgback = findViewById(R.id.icon_volver_R);
+        imgback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
