49febb1b4c3a2bbb849802a15ca8a2722ccf2896
Daniela <97981090+danibarrios22@users.noreply.github.com>
2023-10-25 18:59:38 -0300

    registro y perfil

--- app/src/main/java/com/todook/myapp/Profile.java
+++ app/src/main/java/com/todook/myapp/Profile.java
@@ -1,18 +1,25 @@
 package com.todook.myapp;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.todook.myapp.db.DbHelper;
 
 public class Profile extends AppCompatActivity {
 
     ImageView imgback;
+    Button btnActualize;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
@@ -22,6 +29,21 @@ public class Profile extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Profile.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+
+        btnActualize = findViewById(R.id.btnActualize);
+        btnActualize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DbHelper DbHelper = new DbHelper(Profile.this);
+                SQLiteDatabase db = DbHelper.getWritableDatabase();
+                if(db != null){
+                        Toast.makeText(Profile.this, "Base de datos Creada", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Profile.this, "Error al crear Base de datos ", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
