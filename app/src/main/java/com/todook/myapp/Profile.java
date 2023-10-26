15431c4f3be6059d440e0d2c4974a6df6cfcb936
Edith Moyano <moyanoe357@gmail.com>
2023-10-26 12:51:21 -0300

    hago cambios en profile para cargar imagen. Abre camara

--- app/src/main/java/com/todook/myapp/Profile.java
+++ app/src/main/java/com/todook/myapp/Profile.java
@@ -1,20 +1,43 @@
 package com.todook.myapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity {
 
     ImageView imgback;
+    Button btn_camera;
+    ImageView imgfoto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        btn_camera = findViewById(R.id.btn_camara);
+        imgfoto = findViewById(R.id.imgfoto);
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                camaraLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+            }
+        });
+
+
 
         imgback = findViewById(R.id.icon_volver_R);
         imgback.setOnClickListener(new View.OnClickListener() {
@@ -25,4 +48,19 @@ public class Profile extends AppCompatActivity {
             }
         });
     }
+
+    ActivityResultLauncher<Intent>camaraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode() == RESULT_OK) {
+                Bundle extras = result.getData().getExtras();
+                Bitmap imgBitmap = (Bitmap) extras.get("data");
+                imgfoto.setImageBitmap(imgBitmap);
+            }
+
+        }
+
+    });
+
+
 }
