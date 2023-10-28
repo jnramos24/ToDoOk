6ad350e9efbff1acc479323561ab3d83c5e706f2
nelsonvilte <nelsonvilte@gmail.com>
2023-10-28 17:39:22 -0300

    add options menu

--- app/src/main/java/com/todook/myapp/MainActivity.java
+++ app/src/main/java/com/todook/myapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.todook.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,7 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnContact = findViewById(R.id.btn_contact);
+        Toolbar toolbar = findViewById(R.id.miActionBar);
+        setSupportActionBar(toolbar);
+
+       /* Button btnContact = findViewById(R.id.btn_contact);
         Button btnPerfil = findViewById(R.id.btn_perfil);
         btnContact.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,15 +28,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         btnPerfil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Profile.class);
                 startActivity(intent);
             }
-        });
+        });*/
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_opciones,menu);
+        return true;
     }
 }
