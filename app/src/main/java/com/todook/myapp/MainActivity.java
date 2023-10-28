52f87fb0a2da40095d0b479fe66b9501eb13b011
nelsonvilte <nelsonvilte@gmail.com>
2023-10-28 19:18:55 -0300

    add task recyclerview

--- app/src/main/java/com/todook/myapp/MainActivity.java
+++ app/src/main/java/com/todook/myapp/MainActivity.java
@@ -2,14 +2,24 @@ package com.todook.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.todook.myapp.modelo.Task;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Task> tasks;
+    private RecyclerView rvTasks;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.miActionBar);
         setSupportActionBar(toolbar);
+
+        rvTasks =  findViewById(R.id.rvTasks);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        rvTasks.setLayoutManager(llm);
+        inicializarListaTasks();
+        inicializarAdaptador();
+
 
        /* Button btnContact = findViewById(R.id.btn_contact);
         Button btnPerfil = findViewById(R.id.btn_perfil);
@@ -59,5 +79,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public TaskAdapter adaptador;
+    public void inicializarAdaptador(){
+        adaptador = new TaskAdapter(tasks, this);
+        rvTasks.setAdapter(adaptador);
+    }
+
+    public void inicializarListaTasks(){
+        tasks = new ArrayList<Task>();
+        tasks.add(new Task(1,1,"Tarea 1","12/11/2023","12:00", 1));
+        tasks.add(new Task(2,1,"Tarea 2","10/12/2023","09:00", 1));
+        tasks.add(new Task(3,1,"Tarea 3","05/11/2023","18:00", 2));
+        tasks.add(new Task(4,1,"Tarea 4","25/12/2023","08:00", 3));
+        tasks.add(new Task(5,1,"Tarea 4","01/11/2023","18:00", 1));
+        tasks.add(new Task(6,1,"Tarea 6","01/01/2024","10:00", 2));
+        tasks.add(new Task(7,1,"Tarea 7","30/12/2023","21:00", 3));
     }
 }
