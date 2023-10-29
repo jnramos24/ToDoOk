c9ddd68d96ad2b6c26bd86379842a935e8fce386
nelsonvilte <nelsonvilte@gmail.com>
2023-10-29 14:23:30 -0300

    add tasks table

---
+++ app/src/main/java/com/todook/myapp/db/ConstantesBaseDatos.java
@@ -0,0 +1,12 @@
+package com.todook.myapp.db;
+
+public class ConstantesBaseDatos {
+
+    public static final String TABLE_TASKS           = "task";
+    public static final String TABLE_TASKS_ID        = "id";
+    public static final String TABLE_TASKS_NAME    = "taskname";
+    public static final String TABLE_TASKS_TASKDATE  = "taskdate";
+    public static final String TABLE_TASKS_TIMEDATE     = "timedate";
+    public static final String TABLE_TASKS_TYPE      = "type";
+
+}
