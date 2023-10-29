1be0c7102210712bd6061e63c351fdcf1ac3c3e2
nelsonvilte <nelsonvilte@gmail.com>
2023-10-29 11:40:32 -0300

    update new task activity class

--- app/src/main/java/com/todook/myapp/NewTaskActivity.java
+++ app/src/main/java/com/todook/myapp/NewTaskActivity.java
@@ -1,4 +1,16 @@
 package com.todook.myapp;
 
-public class NewTaskActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewTaskActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task);
+    }
 }
