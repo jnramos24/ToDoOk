9ef4fec593e4721eacc99bf9683a9177294eb81d
Belu Cacciamani <cacciamanibelen@gmail.com>
2023-10-28 22:16:11 -0300

    Creo SplashScreen con logo de app

---
+++ app/src/main/java/com/todook/myapp/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.todook.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int delay = 3000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                startActivity((new Intent(SplashScreen.this, LoginActivity.class)));
+                finish();
+            }
+        }, delay);
+
+
+    }
+}
