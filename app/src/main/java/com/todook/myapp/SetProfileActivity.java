a65bd6344752b982809261f911e51ed64a42a8f3
Daniela <daniela.barrios@dicsys.com>
2023-10-29 22:29:31 -0300

    actualizo profile

---
+++ app/src/main/java/com/todook/myapp/SetProfileActivity.java
@@ -0,0 +1,14 @@
+package com.todook.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_profile);
+    }
+}
